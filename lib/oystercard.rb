49bacbb6f4863a627a797d3cb7b209678beb4fb5
Michael Redgrave <michael@breathehr.com>
2019-07-29 16:16:03 +0100

    add touch in out, journey and minumum balance to card

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,11 +6,41 @@ class Oystercard
 
     def initialize(balance=0)
         @balance = balance
+        @in_journey = false
     end
 
     def topup(value)
         raise "Balance can not be more than #{BALANCE_LIMIT}" if (@balance + value) > BALANCE_LIMIT
         @balance += value
+    end
+
+    def deduct(value)
+        @balance -= value
+    end
+
+    def touch_in
+        raise "Already in journey" if in_journey?
+        raise "Balance needs to be 1 or more" if @balance < 1
+        @in_journey = true
+    end
+
+    def touch_out
+        raise "Already out of journey" if !in_journey?
+        @in_journey = false
+    end
+
+    # def touch
+    #     @in_journey = !@in_journey
+    #     # use the above instead of having the if/else statement below
+    #     # if @in_journey == true
+    #     #     @in_journey = false
+    #     # else 
+    #     #     @in_journey = true
+    #     # end
+    # end
+
+    def in_journey?
+        @in_journey
     end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -18,5 +18,41 @@ describe Oystercard do
         expect{subject.topup(91)}.to raise_error "Balance can not be more than #{BALANCE_LIMIT}"
     end
 
+    it "Has a new balance after deductions applied to card" do
+        subject.topup(10)
+        subject.deduct(5)
+        expect(subject.balance).to eq(5) 
+    end
+
+    describe "#touch_in" do
+        it "raise error if if balance is below 1 when trying to touch in" do
+            expect{subject.touch_in}.to raise_error "Balance needs to be 1 or more"
+        end
+
+
+        it "Change in_journey from false to true" do
+            subject.topup(10)
+            subject.touch_in
+            expect(subject.in_journey?).to eq(true)
+        end
+    end
+
+    describe "#touch_out" do
+        it "Change in_journey from true to false" do
+            subject.topup(10)
+            subject.touch_in
+            subject.touch_out
+            expect(subject.in_journey?).to eq(false)
+        end
+    end
+
+
+    # it "allows customer to touch out if in journey" do
+    #     expect(subject.in_journey?).to eq false
+
+    # end
+
+
+
 
 end
